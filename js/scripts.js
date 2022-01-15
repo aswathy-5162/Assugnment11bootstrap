1bf8de14b1c9651cd804f7121eab632e1fb2160a
aswathy-5162 <aswathy.abhilash5162@gmail.com>
2022-01-15 12:41:56 +0530

    first

---
+++ js/scripts.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for(var i=0;i<=2;i++)
+	// {
+		// head[0].style.backgroundColor='blue';
+	// }
+	// var i=0;
+	// while (i < 3) {
+ //  head[i].style.backgroundColor='blue';
+ //  i++;
+ //   }
